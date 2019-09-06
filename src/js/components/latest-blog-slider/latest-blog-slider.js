f7b33152e107736669b8e24ca66628918cd7dc95
Yaroslav Kulpan <yaroslav.kylpan@gmail.com>
2019-09-06 16:07:39 +0300

    fix

--- src/js/components/latest-blog-slider/latest-blog-slider.js
+++ src/js/components/latest-blog-slider/latest-blog-slider.js
@@ -3,6 +3,9 @@
 $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
+    autoplay:true,
+    autoplayTimeout: 7000,
+    autoplayHoverPause:true,
     responsive: {
         0: {
             items: 1
