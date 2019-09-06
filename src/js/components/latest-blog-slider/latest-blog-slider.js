4460890cad388dc82c745f8f9eeff4d821ea92b5
Yaroslav Kulpan <yaroslav.kylpan@gmail.com>
2019-09-06 14:49:55 +0300

    add validation

--- src/js/components/latest-blog-slider/latest-blog-slider.js
+++ src/js/components/latest-blog-slider/latest-blog-slider.js
@@ -1,1 +1,20 @@
 //= owl.carousel.min.js
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 1
+        },
+        480: {
+            items: 1,
+        },
+        768: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,5 @@
 //= components/modal/modal.js
 //= components/modal-basket.js
 //= components/product-slider/product-slider.js
+//= components/latest-blog-slider/latest-blog-slider.js
 //= common/top-menu.js
