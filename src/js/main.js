d751a4f42783a7844217c85f43879a9217fc0aa0
Valrii_S <deutchep1771@gmail.com>
2019-09-07 19:03:49 +0300

    add filters

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,6 @@
 //= components/modal/modal.js
 //= components/modal-basket.js
 //= components/product-slider/product-slider.js
+//= components/furniture-filters/filter-by-price.js
 //= components/latest-blog-slider/latest-blog-slider.js
 //= common/top-menu.js
