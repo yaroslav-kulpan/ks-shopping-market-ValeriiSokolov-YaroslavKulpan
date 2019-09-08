c6437d4d189dc1795d59ab5c9cd135947f7652c6
Valrii_S <deutchep1771@gmail.com>
2019-09-08 23:23:33 +0300

    add mobile filter modal + fix filter by price

--- src/js/components/furniture-filters/filter-by-price.js
+++ src/js/components/furniture-filters/filter-by-price.js
@@ -1,7 +1,16 @@
 $("#ex12c").slider({id: "slider12c", min: 0, max: 1000, range: true, value: [30, 600]});
-let value = $("#ex12c").slider('getValue');
-$('#rangeValue').text(`$${value[0]} - ${value[1]}`);
+let value1 = $("#ex12c").slider('getValue');
+$('#rangeValue').text(`$${value1[0]} - ${value1[1]}`);
 $("#ex12c").slider().on('slide', function () {
-    let value = $("#ex12c").slider('getValue');
-    $('#rangeValue').text(`$${value[0]} - ${value[1]}`);
+    let value1 = $("#ex12c").slider('getValue');
+    $('#rangeValue').text(`$${value1[0]} - ${value1[1]}`);
 });
+
+$("#ex12b").slider({id: "slider12b", min: 0, max: 1000, range: true, value: [30, 600]});
+let value2 = $("#ex12b").slider('getValue');
+$('#rangeValueModal').text(`$${value2[0]} - ${value2[1]}`);
+$("#ex12b").slider().on('slide', function () {
+    let value2 = $("#ex12b").slider('getValue');
+    $('#rangeValueModal').text(`$${value2[0]} - ${value2[1]}`);
+});
+
