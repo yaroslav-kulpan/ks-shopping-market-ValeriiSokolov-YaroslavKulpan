687d609c80985ce9e51efe3475b3622ca202ae62
kunak <kunak.n@gmail.com>
2019-09-09 15:47:44 +0300

    "navigation"

--- src/js/common/top-menu.js
+++ src/js/common/top-menu.js
@@ -1,22 +1,60 @@
-let btn = document.querySelector('.top-menu .navbar-toggler');
-let btnContainer = document.querySelector('.btn-container');
-// console.log(btn.children);
+$(document).ready(function () {
 
-btn.addEventListener('click', function (event) {
-  event.preventDefault();
-  [].forEach.call(btn.children, function (item) {
-    console.log(item);
-    item.classList.toggle('d-none');
-    // if (item.classList.contains('d-block')) {
-    //   item.classList.remove('d-block');
-    //   item.classList.add('d-none');
-    // }
-    // if (item.classList.contains('d-none')) {
-    //   item.classList.remove('d-none');
-    //   item.classList.add('d-block');
-    // }
+  let btn = $('.top-menu .navbar-toggler');
+  let btnContainer = $('.btn-container');
+
+  btn.on('click', () => $(".top-menu .collapse").collapse('toggle'));
+
+  $(".top-menu .collapse").on('show.bs.collapse', function () {
+    btn.html('<span class="bg-header navbar-toggler-icon"><i class="font-size-large fas fa-times"></i></span>');
   });
 
-  btnContainer.classList.toggle('bg-header');
+  $(".top-menu .collapse").on('hidden.bs.collapse', function () {
+    btn.html('<span class="navbar-toggler-icon"><i class="font-dark font-size-large fas fa-bars"></i></span>');
+  });
 
+  btnContainer.toggleClass('bg-header');
+
+////////////////////////////////////////////////////
+
+  const navLinksCollection = $(`.top-menu .nav-link`);
+  const navItemsCollection = $(`.top-menu .nav-item`);
+  const goTop = $("a[href='#begin']");
+
+  for (let i = 0; i < navLinksCollection.length; i++) {
+    $(navLinksCollection.eq(i)).on('mousedown', function (event) {
+      console.log(this);
+      event.preventDefault();
+      $(".top-menu .collapse").collapse('toggle');
+      $(".top-menu .collapse").on('hidden.bs.collapse', function () {
+        btn.html('<span class="navbar-toggler-icon"><i class="font-dark font-size-large fas fa-bars"></i></span>');
+      });
+
+      navItemsCollection.each((i, item) => $(item).removeClass('active'));
+      $(this).closest('.nav-item').addClass('active');
+
+      $("html, body").animate({
+        scrollTop: $($(this).attr("href")).offset().top
+      }, {
+        duration: 1000,
+      });
+    });
+  }
+
+  //scrollTo(pageX,pageY)
+  goTop.on('click', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000);
+  });
 });
+
+window.onscroll = function () {
+  if (window.pageYOffset > document.documentElement.clientHeight) {
+    $(`.go-top`).addClass(`active`);
+  } else {
+    $(`.go-top`).removeClass(`active`);
+  }
+};
