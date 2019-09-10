420a42a773a6bd893ef8a779c9e4cd6a2280e4fd
kunak <kunak.n@gmail.com>
2019-09-10 14:02:37 +0300

    Merge branch 'master' of C:\Users\mkunak\dev\step-projects\step-project-online-shop with conflicts.

--- src/js/components/working-with-cart/add-product-in-cart.js
+++ src/js/components/working-with-cart/add-product-in-cart.js
@@ -1,6 +1,58 @@
-let toCart = $('.add-to-cart');
-for (let i = 0; i < toCart.length; i++) {
+$(document).ready(function () {
+//= ../product-list.js
+
+  let toCart = $('.add-to-cart');
+/////////////////////////////////////////////////
+  toCart.each((i, item) => {
+    $(item).data('testId', `${products[i].id}`);
+    console.log($(item).data());
+  });
+////////////////////////////////////////////////^
+  for (let i = 0; i < toCart.length; i++) {
     toCart[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      plusCountCart();
+      console.log($(this).data());
+      let img = '';
+      ////////////////////////
+      let priceOld = 0;
+      let priceNew = 0;
+      ///////////////////////^
+      for (let i = 0; i < products.length; i++) {
+        if (products[i].id === $(this).data('id')) {
+          img = products[i].img;
+          ////////////////////////
+          priceOld = products[i].priceOld;
+          priceNew = products[i].priceNew;
+          ///////////////////////^
+        }
+      }
+      $('.modal-empty').remove();
+      $('#cartContainer').append(`<div class="col-12 added-product">
+                               <div class="row d-flex align-items-center">
+                                  <div class="col-4">
+                                    <img src=${img} alt="" class="img-fluid">
+                                   </div>
+                                   <div class="col3">
+                                   <!--/////////////////////////////////////////////-->
+                                   <!--<span class="line-throw-text">$350</span>-->
+                                   <!--<span>$250</span>-->
+                                   <span class="line-throw-text">$`${priceOld}`</span>
+                                   <span>$`${priceNew}`</span>
+                                   <!--/////////////////////////////////////////////^-->
+                                    </div>
+<div class="col-3 d-flex">
+<span class="quantity-text">Quantity</span>
+<span class="quantity-district px-4 py-1">2</span>
+<a href="#" class="btn-plus">+</a>
+<a href="#" class="btn-mines">-</a>
+</div>
+<div class="col-2">
+<span>Sum</span>
+<span>$`${priceNew}`</span>
+</div>
+</div>
+</div>`)
         e.preventDefault();
         plusCountCart();
         let img = '';
@@ -34,9 +86,12 @@ for (let i = 0; i < toCart.length; i++) {
                                     </div>
         `);
     });
-}
+  }
+});
 
 function plusCountCart() {
     let oldCount = parseInt($('#countProductCart').html());
     $('#countProductCart').html(oldCount + 1)
+  let oldCount = parseInt($('#countProductCart').html());
+  $('#countProductCart').html(oldCount + 1)
 }
