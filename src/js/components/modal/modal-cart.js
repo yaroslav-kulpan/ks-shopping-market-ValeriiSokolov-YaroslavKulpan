9c1f72aba4f023c9a72e32c1ae47eca8ddb76ccf
kunak <kunak.n@gmail.com>
2019-09-11 20:44:59 +0300

    "cart modal. done: - works for several product cards inside. todo: - field for total price should be added yet; - css for adaptive design is not on bord yet."

---
+++ src/js/components/modal/modal-cart.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $('.basket .btn-basket').on('click', function () {
+    $("#myModal").modal("show");
+  });
+  // $("#myModal").on('shown.bs.modal', function () {
+    // ../working-with-cart/product-card-counter.js
+  // });
+});
